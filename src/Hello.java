f002e6a791b0b8f1a61791841df1e09e02c89aa5
staer77 <dlwnghks7811@naver.com>
2025-03-20 12:49:47 +0900

    3/20 학습내용

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    class InnerHello {
+        public static void main(String[] args) {
+            //this.this$0 = varl;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
